a1c8b0d44d41c6b3d088d7a001e2f9f015279766
srijanid <srijanidas06@gmail.com>
2024-07-21 12:52:27 +0530

    1st part done

---
+++ portfolio/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import { NavBar } from './components/Navbar';
+import {Banner} from './components/Banner';
+import { Skills } from './components/Skills';
+import { Projects } from './components/Projects';
+
+function App() {
+  return (
+    <div className="App">
+       <NavBar/>
+       <Banner/>
+       <Skills/>
+       <Projects/>
+    </div>
+  );
+}
+
+export default App;
