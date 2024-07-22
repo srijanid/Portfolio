72bf9c965d8c3ed26802a8c1faa99653138803a6
anwghub <anwesa2021@gmail.com>
2024-07-23 01:30:25 +0530

    mail

--- portfolio/server.js
+++ portfolio/server.js
@@ -1,22 +1,25 @@
-const express = require("express");
+require('dotenv').config();
+const express = require('express');
 const router = express.Router();
-const cors = require("cors");
-const nodemailer = require("nodemailer");
+const cors = require('cors');
+const nodemailer = require('nodemailer');
 
-// server used to send send emails
+// server used to send emails
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use("/", router);
-app.listen(5000, () => console.log("Server Running"));
+app.use('/', router);
+app.listen(5000, () => console.log('Server Running'));
+
+// Log the environment variables to verify they are loaded
 console.log(process.env.EMAIL_USER);
 console.log(process.env.EMAIL_PASS);
 
 const contactEmail = nodemailer.createTransport({
   service: 'gmail',
   auth: {
-    user: "********@gmail.com",
-    pass: ""
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
   },
 });
 
@@ -24,19 +27,19 @@ contactEmail.verify((error) => {
   if (error) {
     console.log(error);
   } else {
-    console.log("Ready to Send");
+    console.log('Ready to Send');
   }
 });
 
-router.post("/contact", (req, res) => {
-  const name = req.body.firstName + req.body.lastName;
+router.post('/contact', (req, res) => {
+  const name = `${req.body.firstName} ${req.body.lastName}`;
   const email = req.body.email;
   const message = req.body.message;
   const phone = req.body.phone;
   const mail = {
-    from: name,
-    to: "********@gmail.com",
-    subject: "Contact Form Submission - Portfolio",
+    from: process.env.EMAIL_USER,
+    to: process.env.EMAIL_USER,
+    subject: 'Contact Form Submission - Portfolio',
     html: `<p>Name: ${name}</p>
            <p>Email: ${email}</p>
            <p>Phone: ${phone}</p>
@@ -46,7 +49,9 @@ router.post("/contact", (req, res) => {
     if (error) {
       res.json(error);
     } else {
-      res.json({ code: 200, status: "Message Sent" });
+      res.json({ code: 200, status: 'Message Sent' });
     }
   });
 });
+
+module.exports = router;
